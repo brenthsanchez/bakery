71abac01433a81dc5608da5c283f1e2c59b4e7d8
Brenth Sanchez <brenth_sanchez@hotmail.com>
2015-09-11 14:22:51 -0400

    Revert "full page"
    This reverts commit 2585e3c1a8029cbcffe1ccbc2550f4584072166d.

--- js/mapscroll.js
+++
@@ -1,7 +0,0 @@
-var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-
-var isDraggable = w > 480 ? true : false;
-var mapOptions = {
-  draggable: isDraggable,
-  scrollwheel: false
-};
